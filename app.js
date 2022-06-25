bbe57706314d5339efd53c5fb0fae9726728e889
mmerchut <55309412+mmerchut@users.noreply.github.com>
2022-06-25 20:23:28 +0200

    2 developing

--- app.js
+++ app.js
@@ -1,12 +1,47 @@
 const currentWord = document.querySelector(".current-word");
 const checkWord = document.querySelector('.check');
+const start = document.querySelector('.start');
 const inputWord = document.querySelector("#word");
+let randomWord = '';
+let wordToLearn = "";
+
+const frenchWords = [
+    "je",
+    "de",
+    "est",
+    "pas",
+    "le"
+
+]
+
+const englishWords = [
+    "I",
+    "Of",
+    "Is",
+    "Not",
+    "The"
+]
+
+function getRandomWord() {
+    randomWord = Math.floor(Math.random() * (5 - 1)) + 1;
+    console.log(randomWord);
+    wordToLearn = frenchWords[randomWord];
+     console.log(wordToLearn)
+  }
+
 
 let word = "test"
 
 function checkText() {
+    console.log("1")
+    if(inputWord.value == wordToLearn) {
 
-    console.log("work")
+        console.log('2');
+
+        getRandomWord()
+        console.log(wordToLearn)
+
+    }
 }
 
 function check() {
@@ -14,10 +49,13 @@ function check() {
     if(inputWord.value == word) {
 
         console.log('2');
+        // getRandomWord()
+        // console.log(wordToLearn)
     }
+    
 }
 
 
 inputWord.addEventListener("keydown", checkText);
-
-checkWord.addEventListener('click', check)
+start.addEventListener("click", getRandomWord);
+checkWord.addEventListener('click', check);
