900ff6e15fba78897788a5e92ede44831c7927ba
mmerchut <55309412+mmerchut@users.noreply.github.com>
2022-06-26 12:59:52 +0200

    develop 2

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const currentWord = document.querySelector(".current-word");
+const currentWordEng = document.querySelector(".current-word-eng");
 const checkWord = document.querySelector('.check');
 const start = document.querySelector('.start');
 const inputWord = document.querySelector("#word");
-let randomWord = '';
+let randomWord = "";
 let wordToLearn = "";
+let wordtoLearnEng = "";
 
 const frenchWords = [
     "je",
@@ -38,17 +40,26 @@ function getRandomWord() {
     randomWord = Math.floor(Math.random() * (10 - 1)) + 1;
     console.log(randomWord);
     wordToLearn = frenchWords[randomWord];
+    wordtoLearnEng = englishWords[randomWord];
      console.log(wordToLearn)
      currentWord.textContent = wordToLearn;
+     currentWordEng.textContent = wordtoLearnEng;
+
+     setTimeout(hideCurrentWord, 2000)
   }
 
 
 let word = "test"
 
+function hideCurrentWord() {
+   console.log('1')
+   currentWord.style.display = "none";
+}
+
 function checkText() {
     console.log("1")
-    if(inputWord.value == wordToLearn) {
-
+    if(inputWord.value.toLowerCase() == wordToLearn) {
+        currentWord.style.display = "inline-block";
         console.log('2');
 
         getRandomWord()
